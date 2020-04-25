15521ea5546e3e960e405ffff5fc56bb587e92a4
bikal1000 <bikal1000.bs@gmail.com>
2020-04-25 11:40:47 +0545

    final

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,11 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 2;
+  void changeDiceNumber() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -37,8 +42,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                  rightDiceNumber = Random().nextInt(6) + 1;
+                  changeDiceNumber();
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -48,8 +52,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
                 onPressed: () {
                   setState(() {
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                    leftDiceNumber = Random().nextInt(6) + 1;
+                    changeDiceNumber();
                   });
                 },
                 child: Image.asset('images/dice$rightDiceNumber.png')),
